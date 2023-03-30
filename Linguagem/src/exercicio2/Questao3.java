9a887115422fb9a9e077e627e12a04b13ee5f72e
Luana Braga <108040997+bglassesgirl@users.noreply.github.com>
2023-03-29 21:29:06 -0300

    Add files via upload

---
+++ Linguagem/src/exercicio2/Questao3.java
@@ -0,0 +1,13 @@
+package exercicio2;
+
+public class Questao3 {
+
+	public static void main(String[] args) {
+		
+	for (int cont = 100; cont <=1000; cont++)	{
+		System.out.println(cont);
+	}	
+		
+	}
+}
+
---
+++ Linguagem/src/exercicio2/Questao4.java
@@ -0,0 +1,12 @@
+package exercicio2;
+
+public class Questao4 {
+
+	public static void main(String[] args) {
+		
+		for (int cont = 1000; cont >=100; cont--)	{
+			System.out.println(cont);
+		}	
+			
+  }
+}
